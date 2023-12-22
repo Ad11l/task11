2547c79fa87bfa753a7a2e25a61918aa9c375829
Ad11l <121631664+Ad11l@users.noreply.github.com>
2023-12-22 19:26:04 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Box } from '@mui/system';
-import Header from './Section/Base/index'
+import Header from './Components/Base/index'
+import Buttons from './Components/Base/buttons';
 
 function App() {
   return (
     
     <Box>
     <Buttons/>
-      <TextFields/>
+    <Header/>
     </Box>
   );
 }
